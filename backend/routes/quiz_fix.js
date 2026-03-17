1506d58a8a915afc444e69c214838cd0671c281e
Sai Chandorkar <saichandorkar06@gmail.com>
2026-03-17 23:17:43 +0530

    cleanup: remove duplicate files

--- backend/routes/quiz_fix.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { protect } = require("../middleware/auth");  // ✅ fixed
-const { generateQuiz } = require('../controllers/quizController_fix')
-
-
-router.post("/generate", protect, generateQuiz);
-
-module.exports = router;
